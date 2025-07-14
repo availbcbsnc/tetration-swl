5fc11b1b69c6bdfde4f1f804ba3beb4bdec26522
availbcbsnc <austin.vail@bcbsnc.com>
2025-07-14 13:07:47 -0400

    Add files via upload

---
+++ Scope&Agent-delete/credentials.py
@@ -0,0 +1,10 @@
+# From here put our host that will be used the api
+# example: 
+# API_ENDPOINT='https://endpoint.example.com'
+API_ENDPOINT='https://10.43.54.4'
+# From the API management tab these are the Api key and secret
+# example:
+# API_KEY='c60da3841b8d4809af323f68e736f785'
+# API_SECRET='2bad88888888888888888888888888888'
+API_KEY='396859d8c76a4f40aeb17f2c6bcc8155'
+API_SECRET='e3fab279b1d5b0910dc4d1554c07623ba4896f2b'
